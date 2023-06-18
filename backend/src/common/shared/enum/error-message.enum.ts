2395b7b4d16220e7cd7c3bbc5f18f997d05b991d
JOOYUNHAK <wndbsgkr@naver.com>
2023-06-18 23:24:45 +0900

    add(common): 인증유효시간 초과, 인증번호 불일치 메시지 추가

--- backend/src/common/shared/enum/error-message.enum.ts
+++ backend/src/common/shared/enum/error-message.enum.ts
@@ -1,4 +1,6 @@
 export const enum ErrorMessage {
     PhoneNumberFormat = '휴대폰번호 형식을 확인해주세요',
-    DuplicatedPhoneNumber = '이미 가입된 전화번호입니다.'
+    DuplicatedPhoneNumber = '이미 가입된 전화번호입니다.',
+    ExpiredSmsCode = '인증유효시간이 초과되었습니다. 다시 시도해 주세요.',
+    NotMatchedAuthenticationCode = '인증번호가 일치하지 않습니다.'
 }
