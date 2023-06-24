36ad0ab4f4f80feb83255a4f00be4655912970ac
JOOYUNHAK <wndbsgkr@naver.com>
2023-06-24 23:58:55 +0900

    modify(user-auth-common): user entity method
    enum to char type
    get, set method

--- backend/src/user-auth-common/domain/entity/user.entity.ts
+++ backend/src/user-auth-common/domain/entity/user.entity.ts
@@ -5,6 +5,7 @@ import { UserProfile } from "./user-profile.entity";
 import { Token } from "src/user-auth-common/domain/entity/auth-token.entity";
 import { Time } from "src/common/shared/type/time.type";
 import { Email } from "./user-email.entity";
+import { NewUserAuthentication } from "../interface/new-user-authentication.interface";
 
 @Entity('user')
 export class User {
@@ -14,10 +15,10 @@ export class User {
     @Column({ type: 'varchar', length: 5 })
     private name: string;
 
-    @Column({ type: 'tinyint' })
+    @Column({ type: 'varchar', length: 15 })
     private role: ROLE;
 
-    @Column({ type: 'tinyint' })
+    @Column({ type: 'varchar', length: 10 })
     private loginType: LOGIN_TYPE;
 
     @CreateDateColumn({ name: 'registered_at', type: 'timestamp' })
@@ -32,7 +33,28 @@ export class User {
     @OneToOne(() => UserProfile, (profile) => profile.userId, { cascade: ['insert'] })
     private profile: Promise<UserProfile>;
 
-    @OneToOne(() => Token, (token) => token.userId, { cascade: ['insert', 'update']})
+    @OneToOne(() => Token, (token) => token.userId, { cascade: ['insert', 'update'] })
     private authentication: Promise<Token>;
 
+    constructor( name: string, role: ROLE, loginType: LOGIN_TYPE, email: Email, phone: Phone, profile: UserProfile, authentication: Token ) {
+        this.name = name;
+        this.role = role;
+        this.loginType = loginType;
+        this.email = Promise.resolve(email);
+        this.phone = Promise.resolve(phone);
+        this.profile = Promise.resolve(profile);
+        this.authentication = Promise.resolve(authentication);
+    };
+
+    async getPhone(): Promise<Phone> { return await this.phone; };
+    async getAuthentication(): Promise<Token> { return await this.authentication; };
+    getRole(): ROLE { return this.role; };
+    
+    /* 회원가입시 새로 발급된 인증 */
+    setAuthentication(newUserAuthentication: NewUserAuthentication) { 
+        this.authentication = Promise.resolve(new Token(
+            newUserAuthentication.accessToken,
+            newUserAuthentication.refreshToken
+        ));
+    };
 }
