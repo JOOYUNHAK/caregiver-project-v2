28826770a7426a96151fa21c2414e3ee26e80359
JOOYUNHAK <wndbsgkr@naver.com>
2023-06-26 23:56:18 +0900

    modify(user-auth-common): type const enum to enum
    add reg phone, birth

--- backend/src/user-auth-common/domain/enum/user.enum.ts
+++ backend/src/user-auth-common/domain/enum/user.enum.ts
@@ -1,15 +1,17 @@
 /* 역할(보호자, 간병인) */
-export const enum ROLE { 
+export enum ROLE { 
     PROTECTOR = 'protector', 
     CAREGIVER = 'caregiver'
 }; 
 /* 성별(남, 여) */
-export const enum SEX { 
+export enum SEX { 
     MALE = '남', 
     FEMALE = '여'
 };
 /* 로그인 타입(소셜 로그인 추후 추가) */ 
-export const enum LOGIN_TYPE { 
+export enum LOGIN_TYPE { 
     PHONE = 'phone'
 }; 
 
+export const phoneRegExp = /^\d{3}\d{3,4}\d{4}$/;
+export const birthRegExp = /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/;
