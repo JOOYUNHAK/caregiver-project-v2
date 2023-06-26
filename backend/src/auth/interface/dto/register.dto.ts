9126cc722ce51069a5d5d0a4d09a53910da0f33e
JOOYUNHAK <wndbsgkr@naver.com>
2023-06-27 01:26:26 +0900

    modify: dto validation to class validator

--- backend/src/auth/interface/dto/register.dto.ts
+++ backend/src/auth/interface/dto/register.dto.ts
@@ -1,1 +1,8 @@
-export class RegisterDto { readonly phoneNumber: string; };
+import { Matches } from 'class-validator';
+import { ErrorMessage } from 'src/common/shared/enum/error-message.enum';
+import { phoneRegExp } from 'src/user-auth-common/domain/enum/user.enum';
+
+export class RegisterDto { 
+    @Matches(phoneRegExp, { message: ErrorMessage.PhoneNumberFormat})    
+    readonly phoneNumber: string; 
+};
