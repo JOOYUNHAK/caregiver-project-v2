1dbeb71f0fb02716097c26b591a0838b335b737a
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-03 22:19:01 +0900

    modify: 글로벌 유효성 파이프 미정의 속성 차단 옵션 추가
    whitelist, forbidNonWhiteListed

--- backend/src/common/pipe/global-scoped.pipe.ts
+++ backend/src/common/pipe/global-scoped.pipe.ts
@@ -3,5 +3,5 @@ import { APP_PIPE } from "@nestjs/core";
 
 export const GlobalScopedValidationPipe = {
     provide: APP_PIPE,
-    useValue: new ValidationPipe({ transform: true })
+    useValue: new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true })
 }
