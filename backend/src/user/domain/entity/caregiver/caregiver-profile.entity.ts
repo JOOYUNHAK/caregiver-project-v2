38767b78b4715448f9ece995b06faadd712555ff
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-03 22:29:04 +0900

    add(user): 간병인 프로필, 자격증. 경고 객체 추가
    Mongodb의 필드

---
+++ backend/src/user/domain/entity/caregiver/caregiver-profile.entity.ts
@@ -0,0 +1,48 @@
+import { ObjectId } from 'mongodb'
+import { Warning } from "./warning.entity";
+import { PossibleDate } from "../../enum/possible-date.enum";
+import { CaregiverHelpExperience } from "src/user/interface/dto/register-page";
+import { License } from "./license.entity";
+
+export class CaregiverProfile {
+    readonly _id: ObjectId;
+    private userId: number; // mysql 사용자 계정 ID
+    private weight: number; // 몸무게
+    private career: number; // 경력
+    private pay: number; // 일당
+    private possibleDate: PossibleDate // 가능날짜
+    private nextHosptial: string; // 연장
+    private notice: string; // 공지
+    private helpExperience: CaregiverHelpExperience; //간병 경험
+    private additionalChargeCase: string; //추가 요금 상황
+    private possibleAreaList: string []; // 지역
+    private licenseList: License []; // 자격증
+    private strengthList: string []; // 강점
+    private tagList: string []; // 키워드
+    private warningList: Warning [];
+
+    constructor(id: ObjectId) { this._id = id; };
+
+    /* Setter Method */
+    setUserId(userId: number) { this.userId = userId; };
+    setWeight(weight: number) { this.weight = weight; };
+    setCareer(career: number) { this.career = career; };
+    setPay(pay: number) { this.pay = pay; };
+    setPossibleDate(date: PossibleDate) { this.possibleDate = date; };
+    setNextHosptail(description: string) { this.nextHosptial = description; };
+    setNotice(notice: string) { this.notice = notice; };
+    setHelpExperience(experience: CaregiverHelpExperience) { this.helpExperience = experience; };
+    setAdditionalChargeCase(situation: string) { this.additionalChargeCase = situation; };
+    setPossibleAreaList(areaList: string []) { this.possibleAreaList = areaList; };
+    setLicenseList(licenseList: License []) { this.licenseList = licenseList; };
+    setStrengthList(strengthList: string []) { this.strengthList = strengthList; };
+    setTagList(tagList: string []) { this.tagList = tagList; };
+    setWarning(warningList: Warning []) { this.warningList = warningList; };
+
+    getId(): string { return this._id.toHexString(); };
+    getUserId(): number { return this.userId; };
+    getHelpExperience(): CaregiverHelpExperience { return this.helpExperience; };
+    getLicenseList(): License[] { return this.licenseList; };
+    getStrengthList(): string[] { return this.strengthList; };
+    getWarningList(): Warning[] { return this.warningList; };
+}
---
+++ backend/src/user/domain/entity/caregiver/warning.entity.ts
@@ -0,0 +1,6 @@
+import { Time } from "src/common/shared/type/time.type";
+
+export class Warning {
+    private history: string;
+    private reportedAt: Time;
+}
