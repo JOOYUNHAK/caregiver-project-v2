c46126aa9825cdbd8b832c0250a57dcc7a7da6c2
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-04 20:49:52 +0900

    test: 수정된 보호자 회원가입 Dto 테스트

--- backend/test/unit/user/interface/dto/protector-register.spec.ts
+++ backend/test/unit/user/interface/dto/protector-register.spec.ts
@@ -2,7 +2,7 @@ import { plainToInstance } from "class-transformer"
 import { validate } from "class-validator"
 import { ROLE, SEX } from "src/user-auth-common/domain/enum/user.enum"
 import { ProtectorRegisterDto } from "src/user/interface/dto/protector-register.dto"
-import { CommonRegisterForm, PatientHelpListForm, PatientInfoForm } from "src/user/interface/dto/register-page"
+import { CommonRegisterForm, PatientHelpList, PatientInfoForm } from "src/user/interface/dto/register-page"
 
 describe('보호자 회원가입 전체 Dto Validator Test', () => {
     
@@ -11,8 +11,8 @@ describe('보호자 회원가입 전체 Dto Validator Test', () => {
             ProtectorRegisterDto,
             ProtectorRegisterDto.of(
                 CommonRegisterForm.of('01011111111', '테스트', 19980101, SEX.MALE, ROLE.PROTECTOR),
-                PatientInfoForm.of(60, SEX.MALE, '뇌출혈', new Date(), new Date(), 1, '인천', false, '자세한' ),
-                PatientHelpListForm.of(undefined,'용변', undefined, undefined, undefined, '휠체어')
+                PatientInfoForm.of(new Date(), new Date(), 1),
+                PatientHelpList.of(undefined,'용변', undefined, undefined, undefined, '휠체어')
             )
         );
 
