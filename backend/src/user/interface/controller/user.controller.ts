75b4445dcce2d670a81347d91f5f4447631e4974
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-04 20:52:29 +0900

    modify: UserController 보호자 회원가입 userService로 변경
    registerService -> userService

--- backend/src/user/interface/controller/user.controller.ts
+++ backend/src/user/interface/controller/user.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { ProtectorRegisterDto } from "../dto/protector-register.dto";
-import { RegisterService } from "src/user/application/service/register.service";
 import { ClientDto } from "src/user-auth-common/interface/client.dto";
 import { CaregiverRegisterDto } from "../dto/caregiver-register.dto";
 import { UserService } from "src/user/application/service/user.service";
@@ -9,12 +8,11 @@ import { UserService } from "src/user/application/service/user.service";
 export class UserController {
     constructor(
         private readonly userService: UserService,
-        private readonly registerService: RegisterService
     ) {}
     /* 보호자로 회원가입 */
     @Post('register/protector')
     async registerAsProtector(@Body() protectorRegisterDto: ProtectorRegisterDto): Promise<ClientDto> {
-        return await this.registerService.registerAsProtector(protectorRegisterDto);
+        return await this.userService.register(protectorRegisterDto);
     }
 
     @Post('register/caregiver')
