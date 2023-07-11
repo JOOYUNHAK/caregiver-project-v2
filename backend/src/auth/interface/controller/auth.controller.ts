48ff5922e1e0cb9747e83cc7f82ae02f70514f87
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-12 01:06:57 +0900

    add: AuthController login router 추가

--- backend/src/auth/interface/controller/auth.controller.ts
+++ backend/src/auth/interface/controller/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "src/auth/application/service/auth.service";
 import { RegisterDto } from "../dto/register.dto";
 import { ValidateSmsCodeDto } from "../dto/validate-code.dto";
+import { PhoneAuthenticationSendGuard } from "src/auth/application/guard/authentication-send.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +19,12 @@ export class AuthController {
     @Post('code/sms')
     async validateSmsCode(@Body() validateSmsCodeDto: ValidateSmsCodeDto) {
         return await this.authService.validateSmsCode(validateSmsCodeDto);
+    }
+
+    /* 로그인 */
+    @UseGuards(PhoneAuthenticationSendGuard)
+    @Post('login')
+    async login(@Body('phoneNumber') phoneNumber: string): Promise<'newuser' | 'exist'> {
+        return await this.authService.login(phoneNumber);
     }
 } 
