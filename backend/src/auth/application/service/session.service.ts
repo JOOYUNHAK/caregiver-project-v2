7346a7f3fe94620aa9f38f79131b15e8097b52a5
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-13 02:35:10 +0900

    add: 세션을 관리하는 SessionService 추가

---
+++ backend/src/auth/application/service/session.service.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { RedisClientType } from "redis";
+
+@Injectable()
+export class SessionService {
+    private key: string;
+
+    constructor(
+        @Inject('REDIS_CLIENT')
+        private readonly redis: RedisClientType,
+        private readonly configService: ConfigService
+    ) { this.key = configService.get('db.redis.key.session_list')};
+
+    async addUserToList(userId: number, authentication: string) {
+        await this.redis.HSET(this.key, userId, authentication);
+    }
+}
