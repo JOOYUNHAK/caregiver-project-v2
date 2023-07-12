c959ff1ba5dd272c52c10587f3070195845f115d
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-13 02:33:43 +0900

    add: ErrorMessage Enum 인증 번호, 횟수 관련 메시지 추가

--- backend/src/common/shared/enum/error-message.enum.ts
+++ backend/src/common/shared/enum/error-message.enum.ts
@@ -2,5 +2,7 @@ export const enum ErrorMessage {
     PhoneNumberFormat = '휴대폰번호 형식을 확인해주세요',
     DuplicatedPhoneNumber = '이미 가입된 전화번호입니다.',
     ExpiredSmsCode = '인증유효시간이 초과되었습니다. 다시 시도해 주세요.',
-    NotMatchedAuthenticationCode = '인증번호가 일치하지 않습니다.'
+    NotMatchedAuthenticationCode = '인증번호가 일치하지 않습니다.',
+    ExceededPhoneDailyLimit = '일일 가능한 인증횟수를 초과하였습니다.',
+    ExceededCodeAttempLimit = '인증번호를 연속으로 틀렸습니다.'
 }
