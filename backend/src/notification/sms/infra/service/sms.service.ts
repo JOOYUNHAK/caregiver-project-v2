a898af04ada1831b8b24f8e5d0a8e1851d7c5469
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-13 22:33:01 +0900

    modify: 휴대폰인증 코드 조회 로직 AuthenticationCode 서비스로 이동

--- backend/src/notification/sms/infra/service/sms.service.ts
+++ backend/src/notification/sms/infra/service/sms.service.ts
@@ -1,29 +1,16 @@
-import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { ISMSService } from "../../application/service/isms.service";
 import { NaverSmsService } from "./naver-sms.service";
-import { AuthenticationCodeMessage } from "src/auth/domain/authentication-code-message";
-import { RedisClientType } from "redis";
-import { ErrorMessage } from "src/common/shared/enum/error-message.enum";
-import { AuthenticationCode } from "src/auth/domain/authentication-code";
+import { Message } from "../../domain/message";
 
 
 @Injectable()
 export class SmsService implements ISMSService {
     constructor(
-        @Inject('REDIS_CLIENT')
-        private readonly redis: RedisClientType,
         private readonly naverSmsService: NaverSmsService
     ) {}
 
-    async send(smsMessage: AuthenticationCodeMessage) {
+    async send(smsMessage: Message) {
         await this.naverSmsService.send(smsMessage);
     };
-
-    /* 사용자의 휴대폰 인증 코드 */
-    async getAuthenticationCode(phoneNumber: string): Promise<AuthenticationCode> {
-        const authenticationCode = await this.redis.GET(`phone:${phoneNumber}:code`);
-        if( !authenticationCode )
-            throw new UnauthorizedException(ErrorMessage.ExpiredSmsCode);
-        return new AuthenticationCode(parseInt(authenticationCode));
-    }   
 }
