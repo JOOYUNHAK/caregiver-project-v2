c4fea58fd2ac36b6d607db108184dc841c2bf05f
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-15 00:08:49 +0900

    modify: 클라이언트에 user 상태 저장 코드 수정

--- frontendv2/screens/MyInfo.jsx
+++ frontendv2/screens/MyInfo.jsx
@@ -1,44 +1,21 @@
 /* 마이 페이지 */
 
 import React from 'react';
-import { useState } from 'react';
-import { useLayoutEffect } from 'react';
-import { useEffect } from 'react';
 import {
     View,
     SafeAreaView,
     StyleSheet,
-}
-    from 'react-native';
+} from 'react-native';
 import LoginBox from '../components/MyInfo/LoginBox';
 import StatusBarComponent from '../components/StatusBarComponent';
-import { getLoginState, validateToken } from '../functions/Token';
-import Loading from './Loading';
 
 export default function MyInfo({ navigation }) {
-    const [loading, setLoading] = useState(true);
-    useLayoutEffect(() => {
-        async function getState() {
-            const isLogin = await getLoginState();
-            if (isLogin) {
-                await validateToken(navigation);
-            }
-            setLoading(false)
-        }
-        getState();
-    }, [])
-
     return (
         <SafeAreaView style={styles.container}>
             <StatusBarComponent />
-            {loading ?
-                <Loading /> :
-                <>
-                    <LoginBox navigation={navigation} />
-                    <View style={{ flex: 7 }}>
-                    </View>
-                </>
-            }
+            <LoginBox navigation={navigation} />
+            <View style={{ flex: 7 }}>
+            </View>
         </SafeAreaView>
 
     );
