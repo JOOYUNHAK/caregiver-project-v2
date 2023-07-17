2434d274b71c5e434261b0f09d73fafb6a68e0b4
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-18 03:51:25 +0900

    add: AuthService RefreshAuthentication method 추가

--- backend/src/auth/application/service/auth.service.ts
+++ backend/src/auth/application/service/auth.service.ts
@@ -10,10 +10,16 @@ import { AuthMapper } from "../mapper/auth.mapper";
 import { SessionService } from "./session.service";
 import { AuthenticationCodeService } from "./authentication-code.service";
 import { UserAuthCommonService } from "src/user-auth-common/application/user-auth-common.service";
+import { NewUserAuthentication } from "src/user-auth-common/domain/interface/new-user-authentication.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserRepository } from "src/user-auth-common/domain/repository/user.repository";
+import { RefreshToken } from "src/user-auth-common/domain/refresh-token";
 
 @Injectable()
 export class AuthService {
     constructor(
+        @InjectRepository(User)
+        private readonly userRepository: UserRepository,
         private readonly userAuthCommonService: UserAuthCommonService,
         private readonly authenticationCodeService: AuthenticationCodeService, // 인증 코드 관리 서비스(email, phone)
         private readonly verificationUsageService: VerificationUsageService, // 인증 사용내역 서비스(email, phone)
@@ -37,16 +43,21 @@ export class AuthService {
         return 'newuser';
     }
 
-    async refreshToken(userId: number) {
-        const user = await this.userAuthCommonService.getUser(userId);
+    /* 새로운 인증 갱신 */
+    async refreshAuthentication(user: User): Promise<ClientDto> {
+        const newAuthentication = await this.tokenService.generateNewUsersToken(user); // 새로운 인증 발급
+        user.setAuthentication(newAuthentication); 
+        await this.userRepository.save(user); // 새로운 갱신토큰, 갱신키 저장
+        await this.sessionService.addUserToList(user.getId(), newAuthentication.refreshToken.getToken()); // 새로운 AccessToken 세션에 추가
+        return this.authMapper.toDto(user);
     }
 
     /* 로그인에 성공한 사용자는 사용할 토큰 발급 */
     async createAuthenticationToUser(user: User): Promise<ClientDto> {
-        const accessToken = await this.tokenService.generateAccessToken(user); // 새로운 AccessToken 발급
-        user.setAuthentication({ accessToken, refreshToken: null }); // RefreshToken은 기존 토큰 사용
-        await this.sessionService.addUserToList(user.getId(), accessToken) // sessionList에 추가
-        return await this.authMapper.toDto(user);
+        const newAccessToken = await this.tokenService.generateAccessToken(user); // 새로운 AccessToken 발급
+        user.changeAuthentication(newAccessToken); // RefreshToken은 기존 토큰 사용
+        await this.sessionService.addUserToList(user.getId(), newAccessToken) // sessionList에 추가
+        return this.authMapper.toDto(user);
     }
 
     /* 문자 발송 이후 발송된 코드 저장 */
