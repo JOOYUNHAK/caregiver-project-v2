6fdb38caa98304a7d857fb5c1318beea7a5db452
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-19 23:37:29 +0900

    modify: User Reducer id 필드 삭제

--- frontendv2/redux/reducer/user/userReducer.js
+++ frontendv2/redux/reducer/user/userReducer.js
@@ -2,7 +2,6 @@ import { createReducer } from "@reduxjs/toolkit";
 import { saveUser, logout } from "../../action/user/userAction";
 
 const initialState = {
-    id: '',
     name: ''
 };
 
@@ -12,7 +11,6 @@ const userReducer = createReducer(initialState, (builder) => {
             Object.assign(state, action.payload)
         })
         .addCase(logout, (state) => {
-            state.id = '';
             state.name = '';
         })
 });
