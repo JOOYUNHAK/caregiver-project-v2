d69e9ae6ec746465ad88f7689d002bef713b25ac
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-21 22:56:51 +0900

    add: UserAuthCommonService 사용자 조회 method 추가

--- backend/src/user-auth-common/application/user-auth-common.service.ts
+++ backend/src/user-auth-common/application/user-auth-common.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Phone } from "../domain/entity/user-phone.entity";
 import { PhoneRepository } from "../domain/repository/user-phone.repository";
+import { User } from "../domain/entity/user.entity";
+import { UserRepository } from "../domain/repository/user.repository";
 
 @Injectable()
 export class UserAuthCommonService {
     constructor(
         @InjectRepository(Phone)
-        private readonly phoneRepository: PhoneRepository
+        private readonly phoneRepository: PhoneRepository,
+        @InjectRepository(User)
+        private readonly userRepository: UserRepository
     ) { };
 
     /* 이미 가입되어있는 사용자인지 확인 */
@@ -15,5 +19,9 @@ export class UserAuthCommonService {
         if (await this.phoneRepository.findByPhoneNumber(phoneNumber)) 
             return true;
         return false;
+    }
+
+    async findUserById(userId: number): Promise<User | null> {
+        return await this.userRepository.findById(userId);
     }
 }
