0ddd918a5475d35ff6c550bd36c13fadbd23645a
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-21 22:33:06 +0900

    modify: JwtStrategy validate method 사용자 DB에서 조회

--- backend/src/auth/application/guard/jwt/jwt.strategy.ts
+++ backend/src/auth/application/guard/jwt/jwt.strategy.ts
@@ -1,12 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayload } from "../../type/jwt-payload.type";
+import { User } from "src/user-auth-common/domain/entity/user.entity";
+import { UserAuthCommonService } from "src/user-auth-common/application/user-auth-common.service";
+import { ErrorMessage } from "src/common/shared/enum/error-message.enum";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor( 
+    constructor(
+        private readonly userAuthCommonService: UserAuthCommonService,
         private readonly configService: ConfigService
         ) {
         super({
@@ -16,5 +20,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate(payload: JwtPayload | null) { return payload; };
+    async validate(payload: JwtPayload): Promise<User | never> { 
+        const user = await this.userAuthCommonService.findUserByUserId(payload.userId);
+        if( !user )
+            throw new UnauthorizedException(ErrorMessage.NotExistUser);
+        return user;
+    };
 }
