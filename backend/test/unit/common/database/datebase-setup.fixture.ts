809bfb58ebfbf5b22c604cd99726f65f88b3c9d4
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-21 22:57:54 +0900

    test: TypeOrm Test DB Options setup

--- backend/test/unit/common/database/datebase-setup.fixture.ts
+++ backend/test/unit/common/database/datebase-setup.fixture.ts
@@ -1,5 +1,11 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { Db, MongoClient } from "mongodb";
 import { RedisClientType, createClient } from "redis";
+import { Token } from "src/user-auth-common/domain/entity/auth-token.entity";
+import { Email } from "src/user-auth-common/domain/entity/user-email.entity";
+import { Phone } from "src/user-auth-common/domain/entity/user-phone.entity";
+import { UserProfile } from "src/user-auth-common/domain/entity/user-profile.entity";
+import { User } from "src/user-auth-common/domain/entity/user.entity";
 
 let mongodb: Db, mongoClient: MongoClient; // MongoDB
 
@@ -26,3 +32,15 @@ export async function ConnectRedis() {
 export function getRedis() { return redisClient; };
 
 export async function DisconnectRedis() { await redisClient.disconnect(); };
+
+export const TestTypeOrmOptions: TypeOrmModuleOptions = {
+    type: 'mysql',
+    username: 'root',
+    host: '127.0.0.1',
+    port: 3306,
+    password: '1234',
+    database: 'test_caregiver_project',
+    entities: [User, Phone, Email, UserProfile, Token],
+    synchronize: true,
+    logging: true
+}
