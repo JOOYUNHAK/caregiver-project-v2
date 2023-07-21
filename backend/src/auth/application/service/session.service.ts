95d0b7afd3d36ad60f41f1a8e1d21b7ef39d3073
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-21 23:38:08 +0900

    add: SessionService 세션리스트로부터 사용자 삭제 method

--- backend/src/auth/application/service/session.service.ts
+++ backend/src/auth/application/service/session.service.ts
@@ -12,11 +12,18 @@ export class SessionService {
         private readonly configService: ConfigService
     ) { this.key = configService.get('db.redis.key.session_list')};
 
+    /* 세션리스트에 사용자 추가 */
     async addUserToList(userId: number, authentication: string) {
         await this.redis.HSET(this.key, userId, authentication);
     };
 
+    /* 현재 세션리스트로부터 사용자 조회 */
     async getUserFromList(userId: number) {
         return await this.redis.HGET(this.key, userId.toString());
+    }
+
+    /* 세션리스트로부터 사용자 삭제 */
+    async deleteUserFromList(userId: number) {
+        await this.redis.HDEL(this.key, userId.toString());
     }
 }
