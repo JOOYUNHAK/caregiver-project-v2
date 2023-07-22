57b273b93a84af12663a3269e3d22193b50029c9
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-23 02:21:32 +0900

    add: UserMapper toMyProfileDto method Test

--- backend/test/unit/user/application/mapper/user.mapper.spec.ts
+++ backend/test/unit/user/application/mapper/user.mapper.spec.ts
@@ -1,7 +1,11 @@
+import { ObjectId } from "mongodb";
 import { Token } from "src/user-auth-common/domain/entity/auth-token.entity";
+import { Email } from "src/user-auth-common/domain/entity/user-email.entity";
+import { Phone } from "src/user-auth-common/domain/entity/user-phone.entity";
 import { User } from "src/user-auth-common/domain/entity/user.entity";
 import { LOGIN_TYPE, ROLE, SEX } from "src/user-auth-common/domain/enum/user.enum";
 import { UserMapper } from "src/user/application/mapper/user.mapper"
+import { CaregiverProfileBuilder } from "src/user/domain/builder/profile.builder";
 import { CommonRegisterForm } from "src/user/interface/dto/register-page";
 
 describe('UserMapper Component Test', () => {
@@ -29,6 +33,68 @@ describe('UserMapper Component Test', () => {
             expect(mapResult).toHaveProperty('authentication');
         });
     });
+
+    describe('toMyProfileDto()', () => {
+        describe('내 정보 조회시 간병인 보호자 공통', () => {
+            it('이메일을 등록하지 않은 사용자면 null 설정된다', async() => {
+                const phoneNumber = '01011111111';
+                const testUser = new User(
+                    '테스트',
+                    ROLE.PROTECTOR,
+                    LOGIN_TYPE.PHONE,
+                    null,
+                    new Phone(phoneNumber),
+                    null,
+                    null
+                );
+                const result = await userMapper.toMyProfileDto(testUser);
+
+                expect(result.phoneNumber).toBe(phoneNumber);
+                expect(result.email).toBe(null);
+            });
+
+            it('이메일을 등록한 사용자면 해당 이메일이 설정', async() => {
+                const email = 'test@naver.com';
+                const testUser = new User(
+                    '테스트',
+                    ROLE.PROTECTOR,
+                    LOGIN_TYPE.PHONE,
+                    new Email(email),
+                    new Phone('01011111111'),
+                    null,
+                    null
+                );
+                const result = await userMapper.toMyProfileDto(testUser);
+
+                expect(result.email).toBe(email);
+            })
+        });
+
+        describe('내 정보 조회시 간병인', () => {
+            it('간병인의 결과에는 프로필 비공개여부 필드가 있어야 한다', async() => {
+                const phoneNumber = '01012341234';
+                const testUser = new User(
+                    '간병인',
+                    ROLE.CAREGIVER,
+                    LOGIN_TYPE.PHONE,
+                    null,
+                    new Phone(phoneNumber),
+                    null,
+                    null
+                );
+                const testProfile = new CaregiverProfileBuilder(new ObjectId())
+                                        .isPrivate(false)
+                                        .build();
+
+                const result = await userMapper.toMyProfileDto(testUser, testProfile);
+
+                expect(result.phoneNumber).toBe(phoneNumber);
+                expect(result.email).toBe(null);
+                expect(result.role).toBe(ROLE.CAREGIVER);
+                expect(result.isPrivate).toBe(false);
+            })
+        })
+    })
 
     describe('toDto()', () => {
         it('User 객체에서 사용자에게 넘겨줄 Dto Mapping', async () => {
