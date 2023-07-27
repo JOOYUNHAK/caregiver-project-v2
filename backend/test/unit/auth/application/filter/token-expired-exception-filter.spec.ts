60e0a192797a0851ea5b02ec719d7425c4a7b09c
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-27 23:49:23 +0900

    test: 만료된 토큰 Catch하여 세션 정보에서 삭제하는 에외 필터 Test

---
+++ backend/test/unit/auth/application/filter/token-expired-exception-filter.spec.ts
@@ -0,0 +1,63 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import { TokenExpiredExceptionFilter } from "src/auth/application/filter/token-expired-exception.filter";
+import { SessionService } from "src/auth/application/service/session.service"
+import { ErrorMessage } from "src/common/shared/enum/error-message.enum";
+import { MockJwtService, MockSessionService } from "test/unit/__mock__/auth/service.mock";
+
+describe('TokenExpiredExceptionFilter Test', () => {
+    let sessionService: SessionService;
+    let jwtService: JwtService;
+    let tokenExpiredFilter:TokenExpiredExceptionFilter;
+    
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                MockSessionService,
+                MockJwtService
+            ]
+        }).compile();
+
+        sessionService = module.get(SessionService);
+        jwtService = module.get(JwtService);
+        tokenExpiredFilter = new TokenExpiredExceptionFilter(sessionService, jwtService);
+    });
+
+    it('만료된 토큰으로 인한 문제가 아닌 UnauthorizedException이 발생하면 로직 없이 반환', async () => {
+        
+        const mockRequest: any = {
+            switchToHttp: () => ({
+                getRequest: jest.fn()
+            })
+        };
+
+        const wrongError = new UnauthorizedException('otherMessage');
+
+        const result = await tokenExpiredFilter.catch(wrongError, mockRequest);
+
+        expect(result).toBeUndefined();
+    });
+
+    it('만료된 토큰으로 인한 UnauthorizedException이 발생하면 세션리스트에서 삭제 함수 호출', async () => {
+        const mockRequest: any = {
+            switchToHttp: () => ({
+                getRequest: () => ({
+                    headers: {
+                        authorization: "bearer accessToken"
+                    }
+                })
+            })
+        };
+        
+        const tokenExpiredError = new UnauthorizedException(ErrorMessage.ExpiredToken);
+        
+        const userId = 2;
+        jest.spyOn(sessionService, 'deleteUserFromList').mockResolvedValueOnce(null);
+        jest.spyOn(jwtService, 'decode').mockReturnValueOnce({ userId });
+
+        await tokenExpiredFilter.catch(tokenExpiredError, mockRequest);
+
+        expect(sessionService.deleteUserFromList).toHaveBeenCalledWith(userId);
+    })    
+})
