41137a350853411fa1249b08eec329f93cd9e18d
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-28 01:55:53 +0900

    add: RefreshToken으로 갱신 실패 시 로그아웃 처리하는 공통 함수

---
+++ frontendv2/functions/requestLogout.js
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "../config/CustomAxios";
+import { logout } from "../redux/action/user/userAction";
+import store from "../redux/store";
+
+/* 공통 로그아웃 */
+export default async function requestLogout(navigation) {
+    try {
+        await api.post('auth/logout');
+    }
+    finally {
+        /* AsyncStorage에 있는 값들 모두 지우고  */
+        await AsyncStorage.multiRemove(['accessToken', 'refreshToken', 'name']);
+        store.dispatch(logout());
+        navigation.dispatch(
+            StackActions.push('loginPage')
+        );
+    }
+}
