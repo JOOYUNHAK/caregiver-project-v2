0bccb48ecf0e68571987fa80202dd4a436abb951
JOOYUNHAK <wndbsgkr@naver.com>
2023-07-28 01:55:28 +0900

    add: 로그아웃 버튼 누를 시 로그아웃 처리

--- frontendv2/components/MyProfile/UserLogin/Logout.jsx
+++ frontendv2/components/MyProfile/UserLogin/Logout.jsx
@@ -1,33 +1,38 @@
 /* 내 계정 관리 logout 부분 */
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { StackActions } from "@react-navigation/native";
 import { Text } from "react-native";
 import { StyleSheet } from "react-native";
 import { View } from "react-native";
 import { TouchableHighlight } from "react-native";
-import { heightPercentageToDP as hp} from "react-native-responsive-screen";
-import { useDispatch } from "react-redux";
-import { resetHeartList } from "../../../redux/action/profile/profileAction";
-import { saveIsAuthed } from "../../../redux/action/register/firstRegisterAction";
+import { useDispatch } from 'react-redux';
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import api from "../../../config/CustomAxios";
 import { logout } from "../../../redux/action/user/userAction";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StackActions } from "@react-navigation/native";
 
 export default function Logout({ navigation }) {
     const dispatch = useDispatch();
+
+    /* 로그아웃 시도 시 에러가 나면 무조건 로그아웃 처리 */
+    const pressLogoutBtn = async () => {
+        try {
+            await api.post('auth/logout');
+        }
+        catch (err) {
+            dispatch(logout()); //현재 유저정보 리셋
+            await AsyncStorage.multiRemove(['accessToken', 'refreshToken', 'name']);
+            navigation.dispatch(
+                StackActions.pop()
+            );
+        }
+    }
 
     return (
         <TouchableHighlight
             style={{ heigh: hp('6%') }}
             underlayColor='none'
-            onPress={async () => {
-                dispatch(logout()); //현재 유저정보 리셋
-                dispatch(saveIsAuthed(false)); //휴대폰 인증 여부 리셋
-                dispatch(resetHeartList()); // 찜 목록 초기화
-                await AsyncStorage.multiRemove(['accessToken', 'refreshToken']);
-                navigation.dispatch(
-                    StackActions.pop()
-                );
-            }}
+            onPress={async () => await pressLogoutBtn(navigation)}
         >
             <View style={styles.eachPart}>
                 <Text style={{ fontSize: 16, fontWeight: '400' }}>
