a596e893d420c1b0783ba3eca11de2aeab5db55c
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-02 01:50:35 +0900

    add: 간병인 프로필 저장소에 cursor반환하는 getProfileList() 추가

--- backend/src/profile/infra/repository/caregiver-profile.repository.ts
+++ backend/src/profile/infra/repository/caregiver-profile.repository.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { plainToInstance } from "class-transformer";
-import { Db, ObjectId, WithId } from "mongodb";
+import { AggregationCursor, Db, Document, ObjectId, WithId } from "mongodb";
 import { CaregiverProfile } from "src/profile/domain/entity/caregiver/caregiver-profile.entity";
 import { ICaregiverProfileRepository } from "src/profile/domain/repository/icaregiver-profile.repository";
 
@@ -28,7 +28,7 @@ export class CaregiverProfileRepository implements ICaregiverProfileRepository<W
                                 .collection<CaregiverProfile>(this.collectionName)
                                 .findOne({ _id: new ObjectId(id) });
         
-        return plainToInstance(CaregiverProfile, findProfile); 
+        return this.documentToInstance(findProfile);
     }
 
     async findByUserId(userId: number): Promise<CaregiverProfile> {
@@ -37,12 +37,53 @@ export class CaregiverProfileRepository implements ICaregiverProfileRepository<W
                                 .aggregate()
                                 .match({ userId })
                                 .toArray();
-        return plainToInstance(CaregiverProfile, findProfile);
+        
+        return this.documentToInstance(findProfile);
     }
 
     async delete(id: string): Promise<void> {
         await this.mongodb
             .collection<CaregiverProfile>(this.collectionName)
             .deleteOne({ _id: new ObjectId(id) });
+    }
+
+    /* 데이터에 접근할 수 있는 Cursor 반환 */
+    getProfileList(lastProfileId?: string): AggregationCursor {
+        return this.mongodb
+                .collection(this.collectionName)
+                .aggregate(this.profileListPipelineStage(lastProfileId))
+                .batchSize(20)
+    }
+
+    /* 조회된 Document를 인스턴스로 변경 */
+    private documentToInstance(document: WithId<CaregiverProfile> | Document): CaregiverProfile {
+        return plainToInstance(CaregiverProfile, document);
+    }
+
+    /* 프로필 리스트 조회하는 Stage */
+    private profileListPipelineStage = (lastProfileId: string) => [
+        { $match: 
+            {
+            ...this.ltProfileId(lastProfileId),
+            isPrivate: false
+            }
+        },
+        { $sort: { _id: -1 }},
+        { $project: {
+            userId: 1,
+            career: 1,
+            pay: 1,
+            notice: 1,
+            possibleDate: 1,
+            possibleAreaList: 1,
+            tagList: 1
+        }}
+    ]
+
+    /* 마지막 프로필 아이디보다 오래된 프로필들 받아오기 위해 */
+    private ltProfileId(profileId: string): null | { _id: { $lt: ObjectId }} {
+        if( !profileId ) return null;
+
+        return { _id: { $lt: new ObjectId(profileId) } };
     }
 }
