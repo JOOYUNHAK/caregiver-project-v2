8bb46f160d034b54a3729aade588e838bbee7c8d
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-02 02:45:45 +0900

    add: 간병인 프로필 Service getProfileList() 메서드 추가

--- backend/src/profile/application/service/caregiver-profile.service.ts
+++ backend/src/profile/application/service/caregiver-profile.service.ts
@@ -3,13 +3,19 @@ import { CaregiverRegisterDto } from "src/profile/interface/dto/caregiver-regist
 import { CaregiverProfileMapper } from "../mapper/caregiver-profile.mapper";
 import { CaregiverProfileRepository } from "src/profile/infra/repository/caregiver-profile.repository";
 import { CaregiverProfile } from "src/profile/domain/entity/caregiver/caregiver-profile.entity";
+import { concatMap, firstValueFrom, from, mergeMap, toArray } from "rxjs";
+import { UserAuthCommonService } from "src/user-auth-common/application/user-auth-common.service";
+import { plainToInstance } from "class-transformer";
+import { ProfileListDto } from "src/profile/interface/dto/profile-list.dto";
 
 @Injectable()
 export class CaregiverProfileService {
     constructor(
         private readonly caregiverProfileMapper: CaregiverProfileMapper,
-        private readonly caregiverProfileRepository: CaregiverProfileRepository
+        private readonly caregiverProfileRepository: CaregiverProfileRepository,
+        private readonly userCommonService: UserAuthCommonService
     ) {}
+    
     /* 회원가입시 새로운 프로필 추가 */
     async addProfile(userId: number, caregiverRegisterDto: CaregiverRegisterDto): Promise<void> {
         const caregiverProfile = this.caregiverProfileMapper.mapFrom(userId, caregiverRegisterDto);
@@ -19,5 +25,25 @@ export class CaregiverProfileService {
     /* 사용자 아이디로 프로필 조회 */
     async getProfileByUserId(userId: number): Promise<CaregiverProfile> {
         return await this.caregiverProfileRepository.findByUserId(userId)
+    }
+
+    /* 프로필 리스트 조회 */
+    async getProfileList(lastProfileId?: string): Promise<ProfileListDto []> {
+        const profileCursor = this.caregiverProfileRepository.getProfileList(lastProfileId);
+
+        return await firstValueFrom(
+            from(profileCursor)
+            .pipe(
+                concatMap(async (profile: CaregiverProfile) => 
+                    await this.fetchUserAndMerge(plainToInstance(CaregiverProfile, profile))),
+                toArray()
+            )
+        )
+    }
+ 
+    /* 사용자 id를 받아오면서 profile 형식에 맞춰서 변경 */
+    private async fetchUserAndMerge(profile: CaregiverProfile): Promise<ProfileListDto> { 
+        const user = await this.userCommonService.findUserById(profile.getUserId());
+        return this.caregiverProfileMapper.toListDto(user, profile);
     }
 }
