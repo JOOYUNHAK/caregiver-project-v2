1b7c12f32e28c7a9b36a6ef322787625d2e01f48
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-03 15:23:50 +0900

    modify: Redis localhost에서 Redis Cloud로 변경

--- backend/src/common/shared/database/redis/redis.providers.ts
+++ backend/src/common/shared/database/redis/redis.providers.ts
@@ -1,10 +1,19 @@
+import { ConfigService } from "@nestjs/config";
 import { createClient } from "redis"
 
 export const redisProviders = [
     {
+        inject: [ConfigService],
         provide: 'REDIS_CLIENT',
-        useFactory: async() => {
-            const client = createClient();
+        useFactory: async(configService: ConfigService) => {
+            const client = createClient({
+                username: configService.get('db.redis.acl.username'),
+                password: configService.get('db.redis.acl.password'),
+                socket: {
+                    port: configService.get('db.redis.acl.port'),
+                    host: configService.get('db.redis.acl.host')
+                }
+            })
             await client.connect();
             return client;
         }
