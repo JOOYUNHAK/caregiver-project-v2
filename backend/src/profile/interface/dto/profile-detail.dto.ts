eba42a5ac9ee2c53650c0c916172d8a45e1d8758
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-08 23:40:22 +0900

    add: 프로필 상세페이지 Dto Interface 추가

---
+++ backend/src/profile/interface/dto/profile-detail.dto.ts
@@ -0,0 +1,26 @@
+import { Warning } from "src/profile/domain/entity/caregiver/warning.entity";
+import { SEX } from "src/user-auth-common/domain/enum/user.enum"
+import { CaregiverHelpExperience } from "src/user/interface/dto/register-page";
+
+export interface ProfileDetailDto {
+    user: {
+        name: string;
+        sex: SEX;
+        age: number;
+    },
+    profile: {
+        _id: string;
+        userId: number;
+        career: string;
+        pay: number;
+        possibleDate: string;
+        possibleAreaList: string[] | string;
+        notice: string;
+        licenseList: string[] | [];
+        additionalChargeCase: string | null;
+        nextHosptial: string | null;
+        helpExperience: CaregiverHelpExperience,
+        strengthList: string[] | [],
+        warningList: Warning [] | []
+    }
+}
