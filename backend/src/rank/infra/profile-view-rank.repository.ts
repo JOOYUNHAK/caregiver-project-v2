9ed63e04b1a2e650abd05e2acedd0592bf4df117
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-11 00:03:46 +0900

    add: 프로필 조회에 대한 랭킹을 매기는 저장소 추가

---
+++ backend/src/rank/infra/profile-view-rank.repository.ts
@@ -0,0 +1,21 @@
+import { RedisClientType } from "redis";
+import { IRankRepository } from "../domain/irank.repository";
+import { Inject, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class ProfileViewRankRepository implements IRankRepository {
+    private rankKey: string;
+
+    constructor(
+        @Inject('REDIS_CLIENT')
+        private readonly redis: RedisClientType,
+        private readonly configService: ConfigService
+    ) {
+        this.rankKey = configService.get('profile_view_rank_key');
+    };
+
+    async increment(profileId: string): Promise<void> {
+        await this.redis.ZINCRBY(this.rankKey, 1, profileId);
+    }
+}
