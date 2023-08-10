cc10b3c6e108378142be4e397ea6ad9c524daf71
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-11 01:06:02 +0900

    add: ProfileController 프로필 상세조회 router 추가

--- backend/src/profile/interface/controller/profile.controller.ts
+++ backend/src/profile/interface/controller/profile.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Param,  Query } from "@nestjs/common";
 import { Public } from "src/auth/application/decorator/public.decorator";
 import { CaregiverProfileService } from "src/profile/application/service/caregiver-profile.service";
 import { ProfileListDto } from "../dto/profile-list.dto";
+import { AuthenticatedUser } from "src/auth/application/decorator/user.decorator";
+import { User } from "src/user-auth-common/domain/entity/user.entity";
 
 @Controller('profile')
 export class ProfileController {
@@ -14,5 +16,10 @@ export class ProfileController {
     @Get('list')
     async getProfileList(@Query('lastProfileId') lastProfileId?: string): Promise<ProfileListDto []> {
         return await this.caregiverProfileService.getProfileList(lastProfileId);
+    }
+
+    @Get('detail/:id')
+    async getProfileDetail(@Param('id') profileId: string, @AuthenticatedUser() authenticatedUser: User){
+        return await this.caregiverProfileService.getProfile(profileId, authenticatedUser)
     }
 }
