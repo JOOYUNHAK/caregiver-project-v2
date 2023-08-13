a9cf933be47ae57bb70fe7964f67ccec2012c556
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-13 23:47:49 +0900

    modify: RequestUserProfile() 함수 파라미터 purpose삭제

--- frontendv2/api/Profile/requestUserProfile.jsx
+++ frontendv2/api/Profile/requestUserProfile.jsx
@@ -1,34 +1,28 @@
-import { useSelector } from "react-redux";
 import api from "../../config/CustomAxios";
+import { requestRefreshToken } from "../../functions/Token";
 import { saveUserProfile } from "../../redux/action/profile/profileAction";
 import { saveMostViewed } from "../../redux/action/search/searchAction";
 import store from "../../redux/store";
 
-export default async function requestUserProfile(purpose, profileId) {
-    const userId = store.getState().user.id;
+export default async function requestUserProfile(navigation, profileId) {
     let { mostViewed } = store.getState().search;
     mostViewed = mostViewed ? true : undefined;
     try {
-        //const start = new Date().getTime();
-        const res = await api.get(`user/profile/${purpose}`, {
-            params: {
-                userId: userId,
-                profileId: profileId,
-                mostViewed: mostViewed
-            }
-        })
-        //const end = new Date().getTime();
-        //console.log(end - start)
+        const res = await api.get(`profile/detail/${profileId}`);
         store.dispatch(saveUserProfile(res.data));
         store.dispatch(saveMostViewed(false));
-        return 'true';
+        return true;
     }
     catch (err) {
-        console.log(err.response)
+        console.log(err.response.data)
         const status = err.response.status;
         const message = err.response.data.message;
         if (status == 404) {
             return message;
         }
+        await requestRefreshToken(navigation);
+        const res = await api.get(`profile/detail/${profileId}`);
+        store.dispatch(saveUserProfile(res.data));
+        return true;
     }
 }
