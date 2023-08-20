43f8edd5c984c44d8c558568cedd97d0b8178cdf
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-20 18:19:42 +0900

    modify: ProfileLIstCursor에 반환할 다음커서 생성해주는 메서드 추가

--- backend/src/profile/domain/profile-list.cursor.ts
+++ backend/src/profile/domain/profile-list.cursor.ts
@@ -1,10 +1,10 @@
+import { CaregiverProfileListData } from "./profile-list-data";
+import { ProfileListQueryOptions } from "./profile-list-query-options";
+
 export class ProfileListCursor {
     private next?: string;
 
     constructor(next?: string) { this.next = next; };
-
-    /* 기본 최신순 제외하고 다른 정렬이 선택됐는지 */
-    hasCombinedSortConditions(): boolean { return this.next && this.next.includes('_') };
 
     /* 기본 정렬 제외한 다른 정렬의 시작 커서 값 */
     combinedOtherSortNext(): number { return parseInt( this.next.split('_')[0] ); };
@@ -12,8 +12,33 @@ export class ProfileListCursor {
     /* 기본 정렬의  */
     combinedDefaultSortNext(): string { return this.next.split('_')[1]; };
 
-    /* 다음 프로필 아이디 */
+    /* 요청에서 넘어온 cursor 값 */
     defaultSortNext(): string | undefined { return this.next; };
 
+    /* 클라이언트에게 넘겨줄 cursor 값 */
+    toClientNext(): string | null { return this.next; };
+
+    /* 다음 커서 생성 */
+    static createNextCursor(
+        profileList: CaregiverProfileListData [],
+        queryOptions: ProfileListQueryOptions
+    ) {
+        if( !profileList.length ) return new ProfileListCursor(null);
+        
+        const lastProfile = profileList.at(-1);
+        
+        let nextCursor = queryOptions.getSortOptions().hasOptions() ? 
+                this.createCombinedCursor(lastProfile, queryOptions.getSortOptions().otherField()) : lastProfile.profile.id;
+        
+        return new ProfileListCursor(nextCursor)
+    }
+
+    private static createCombinedCursor(lastProfile: CaregiverProfileListData, otherSortField: string) {
+        const otherSortFieldLastValue = otherSortField === 'pay' ? 
+            lastProfile.profile.pay : lastProfile.profile.possibleDate;
+        return `${otherSortFieldLastValue}_${lastProfile.profile.id}`;
+    }
+
+    /* 테스트용 */
     static of(next?: string): ProfileListCursor { return new ProfileListCursor(next); };
 }
