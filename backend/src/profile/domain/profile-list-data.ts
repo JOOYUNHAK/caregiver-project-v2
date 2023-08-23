682383001fce32a2041e7e7a5eb769ba1e3c01ee
JOOYUNHAK <wndbsgkr@naver.com>
2023-08-24 00:55:04 +0900

    add:클라이언트를 위한 인터페이스 ProfileListDataAsClient 추가

--- backend/src/profile/domain/profile-list-data.ts
+++ backend/src/profile/domain/profile-list-data.ts
@@ -1,4 +1,20 @@
+// DB에서 조회되는 데이터 형식
 export interface CaregiverProfileListData {
+    id: string;
+    userId: number;
+    name: string;
+    sex: string;
+    age: number;
+    career: number;
+    pay: number;
+    possibleDate: number;
+    possibleAreaList: string[];
+    tagList: string[];
+    notice: string;
+};
+
+// 프론트엔드에서 필요한 형식으로 변환
+export interface ProfileListDataAsClient {
     id: string;
     userId: number;
     name: string;
@@ -10,4 +26,4 @@ export interface CaregiverProfileListData {
     possibleAreaList: string[] | string;
     tagList: string[];
     notice: string;
-};
+}
