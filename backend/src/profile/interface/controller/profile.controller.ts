b635fd071324f446ec11a8a2de83a8613e9df2de
JOOYUNHAK <wndbsgkr@naver.com>
2023-09-02 14:57:47 +0900

    add: ProfileController에 프로필 조회 내역 추가 Router 추가

--- backend/src/profile/interface/controller/profile.controller.ts
+++ backend/src/profile/interface/controller/profile.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param,  Post,  Query, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param,  Patch,  Post,  Query, UseGuards } from "@nestjs/common";
 import { Public } from "src/auth/application/decorator/public.decorator";
 import { CaregiverProfileService } from "src/profile/application/service/caregiver-profile.service";
 import { ProfileListDto } from "../dto/profile-list.dto";
@@ -9,13 +9,16 @@ import { Roles } from "src/common/shared/decorator/roles.decorator";
 import { ROLE } from "src/user-auth-common/domain/enum/user.enum";
 import { ProfileLikeHistoryService } from "src/profile/application/service/profile-like-history.service";
 import { RoleGuard } from "src/core/guard/role.guard";
+import { ProfileViewRankService } from "src/rank/application/service/profile-view-rank.service";
 
 @Controller('profile')
 export class ProfileController {
 
     constructor(
         private readonly caregiverProfileService: CaregiverProfileService,
-        private readonly profileLikeHistoryService: ProfileLikeHistoryService
+        private readonly profileLikeHistoryService: ProfileLikeHistoryService,
+        private readonly profileViewRankService: ProfileViewRankService
+
     ) {};
 
     @Public()
@@ -25,8 +28,18 @@ export class ProfileController {
     }
 
     @Get('detail/:id')
-    async getProfileDetail(@Param('id') profileId: string, @AuthenticatedUser() authenticatedUser: User){
-        return await this.caregiverProfileService.getProfile(profileId, authenticatedUser)
+    async getProfileDetail(@Param('id') profileId: string){
+        return await this.caregiverProfileService.getProfile(profileId)
+    }
+
+    @Patch(':profileId/view')
+    async addViewHistory(
+        @Param('profileId') profileId: string, 
+        @AuthenticatedUser() authenticatedUser: User
+    ) {
+        /* 사용자가 보호자라면 증가 */
+        if( authenticatedUser.getRole() === ROLE.PROTECTOR )
+            return await this.profileViewRankService.increment(profileId, authenticatedUser);
     }
 
     @Post('like')
