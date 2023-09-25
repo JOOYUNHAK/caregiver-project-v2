d9c356cab807188d654dd8c6a758a1a94f140b39
JOOYUNHAK <wndbsgkr@naver.com>
2023-09-25 17:49:34 +0900

    add: ChatFixtures에 Message 생성 Method 추가

--- backend/test/unit/chat/chat.fixtures.ts
+++ backend/test/unit/chat/chat.fixtures.ts
@@ -5,9 +5,25 @@ import { ApplicationText } from "src/chat/domain/enum/application-text.enum";
 import { MessageType } from "src/chat/domain/enum/chat-message-type.enum";
 
 export class ChatFixtures {
+
     static createRoom(memberId1: number, memberId2: number): ChatRoom {
         const message = new ChatMessage(memberId1, memberId2, MessageType.APPLICATION, ApplicationText.REQUESTED);
         const room = ChatRoom.start(new Participant(memberId1), new Participant(memberId2), message);
         return room;
+    };
+
+    /* 일반 텍스트 형태의 메시지 생성 */
+    static createTextMessage(memberId1: number, memberId2: number): ChatMessage {
+        const message = new ChatMessage(memberId1, memberId2, MessageType.TEXT, '테스트');
+        return message;
+    }
+
+    /* 신청서 메시지 생성 */
+    static createApplicationMessage(
+        memberId1: number, memberId2: number, applicationId: number
+    ): ChatMessage {
+        const message = new ChatMessage(memberId1, memberId2, MessageType.APPLICATION, ApplicationText.REQUESTED)
+                        .withApplicationCode(applicationId);
+        return message;
     }
 }
