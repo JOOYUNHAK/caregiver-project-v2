92ba23b326ff5e1d7aabe29c9a9d67cd6421ec12
JOOYUNHAK <wndbsgkr@naver.com>
2023-09-27 22:50:50 +0900

    add: 이름, 신청서 상태, 방 정보 조회 결과 Dto Test Method 추가

--- backend/src/user-auth-common/domain/interface/get-name.dto.ts
+++ backend/src/user-auth-common/domain/interface/get-name.dto.ts
@@ -1,4 +1,12 @@
-export interface GetNameDto {
+export class GetNameDto {
     id: number;
     name: string;
+
+    /* Test */
+    static of(userId: number, name: string) {
+        return { 
+            id: userId,
+            name
+        }
+    }
 }
