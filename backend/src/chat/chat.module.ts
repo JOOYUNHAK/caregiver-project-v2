1acdcf348bc9981dbe3061d94da37a05671b3995
JOOYUNHAK <wndbsgkr@naver.com>
2023-09-28 02:22:52 +0900

    add: ChatModule, CareApplication Module 추가

---
+++ backend/src/chat/chat.module.ts
@@ -0,0 +1,35 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CARE_APPLIED_SERVICE } from "src/common/shared/constants";
+import { ChatRoom } from "./domain/entity/chat-room.entity";
+import { ChatMessage } from "./domain/entity/chat-message.entity";
+import { Participant } from "./domain/entity/participant.entity";
+import { ApplicationCode } from "./domain/entity/application-code.entity";
+import { CareAppliedService } from "./application/service/care-applied.service";
+import { UserAuthCommonModule } from "src/user-auth-common/user-auth-common.module";
+import { ChatController } from "./interface/controller/chat.controller";
+import { ChatRoomService } from "./application/service/chat-room.service";
+import { CareApplicationModule } from "src/care-application/care-application.module";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            ChatRoom, ChatMessage, Participant, ApplicationCode
+        ]),
+        UserAuthCommonModule,
+        CareApplicationModule
+    ],
+    controllers: [ChatController],
+    providers: [
+        {
+            provide: CARE_APPLIED_SERVICE,
+            useClass: CareAppliedService
+        },
+        ChatRoomService,
+    ],
+    exports: [
+        CARE_APPLIED_SERVICE
+    ]
+})
+export class ChatModule {}
+
