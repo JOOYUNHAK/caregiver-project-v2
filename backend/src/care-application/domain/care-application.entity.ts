d84d3ab73a0dca7764e8454e9ccb560a46a13dc1
JOOYUNHAK <wndbsgkr@naver.com>
2023-10-10 00:37:55 +0900

    modify: CareApplication Entity isCompleted()로 변경

--- backend/src/care-application/domain/care-application.entity.ts
+++ backend/src/care-application/domain/care-application.entity.ts
@@ -1,8 +1,6 @@
 import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { ApplicationStatus } from "../../chat/domain/enum/application-status.enum";
 import { Time } from "src/common/shared/type/time.type";
-import { ConflictException } from "@nestjs/common";
-import { ErrorMessage } from "src/common/shared/enum/error-message.enum";
 
 @Entity('care_application')
 @Index(["protectorId", "caregiverId"])
@@ -39,11 +37,8 @@ export class CareApplication {
     watched() { this.status = ApplicationStatus.WATCHED; };
     rejected() { this.status = ApplicationStatus.REJECTED; };
 
-    /* 해당 사용자가 신청한 내역 중 진행중인 내역이 있는지 */
-    validate(): void {
-        if( 
-            this.status == ApplicationStatus.REQUESTED ||
-            this.status == ApplicationStatus.WATCHED
-        ) throw new ConflictException(ErrorMessage.AlreadyHavePendingApplication);
+    /* 해당 사용자가 가장 최근에 신청한 내역이 완료됐는지 */
+    isCompleted(): boolean {
+        return (this.status == ApplicationStatus.REQUESTED || this.status == ApplicationStatus.WATCHED ) ? false : true;
     }
 }
